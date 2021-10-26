77c9fa5a853143adb4ad01f0694e7dd233c4f9b0
rocktomic <67294136+rocktomic@users.noreply.github.com>
2021-10-26 16:56:01 -0400

    Create conversionTracking.js

---
+++ conversion/conversionTracking.js
@@ -0,0 +1,16 @@
+function PostRocktomicConversion(eventName) {
+	var utmSource = window.localStorage.getItem('utm_source');
+	var utmMedium = window.localStorage.getItem('utm_medium');
+	var utmCampaign = window.localStorage.getItem('utm_campaign');
+	var utmTerm = window.localStorage.getItem('utm_term');
+	var utmContent = window.localStorage.getItem('utm_content');
+
+	var settings = {
+		"url": "https://hooks.zapier.com/hooks/catch/4392881/bhi3c7x",
+		"type": "POST",
+		"data": { "event": eventName, "utm_source": utmSource, "utm_medium": utmMedium, "utm_campaign": utmCampaign, "utm_term": utmTerm, "utm_content": utmContent },
+		"timeout": 0,
+	};
+
+	$.ajax(settings);
+}
